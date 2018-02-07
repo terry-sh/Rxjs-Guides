8c1da8eed2f17b012017956cf559fe45b9ecc50f
o-jules <jules3in@gmail.com>
2018-02-07 23:12:22 +0800

    add sample file: subscription.js

---
+++ samples/src/subscription.js
@@ -0,0 +1,20 @@
+var Rx = require('rxjs')
+
+var observable1 = Rx.Observable.interval(400);
+var observable2 = Rx.Observable.interval(300);
+
+var subscription = observable1.subscribe(x => console.log('first: ' + x));
+var childSubscription = observable2.subscribe(x => console.log('second: ' + x));
+
+subscription.add(childSubscription);
+
+setTimeout(() => {
+  // Unsubscribes BOTH subscription and childSubscription
+  subscription.unsubscribe();
+}, 1000);
+
+// second: 0
+// first: 0
+// second: 1
+// first: 1
+// second: 2
