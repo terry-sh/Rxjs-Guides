ddb33e937235771aeeeee2299362cfb9b0e1ff9f
o-jules <jules3in@gmail.com>
2018-02-27 21:08:54 +0800

    update: behaior-subject.js, add Axios for ajax request

--- samples/src/behavior-subject.js
+++ samples/src/behavior-subject.js
@@ -1,4 +1,5 @@
 var Rx = require('rxjs')
+var Axios = require('axios')
 
 /**
  * 使用 BehaviorSubject 作全局的 store
@@ -6,8 +7,9 @@ var Rx = require('rxjs')
 
 function initialUserState() {
 	return {
-		id: 1,
-		name: 'store'
+		name: 'weather',
+		isLoading: false,
+		provinces: []
 	}
 }
 
@@ -28,19 +30,65 @@ function createStore(initialState) {
 
 const store = createStore(initialUserState)
 
-const s1 = store.subscribe(val => {
-	console.log('s1', val)
+const s1 = store.subscribe(state => {
+	console.log('s1', state)
 })
 
 store.next(state => ({
-	id: 2
+	name: 'real-time weather'
 }))
 
-const s2 = store.subscribe(val => {
-	console.log('s2', val)
+const s2 = store.subscribe(state => {
+	console.log('s2', state)
 })
 
 store.next(state => ({
-	id: 3,
-	name: 'store 3'
+	name: 'china weather'
 }))
+
+const getProvince = () => {
+	store.next(state => ({
+		isLoading: true
+	}))
+
+	Rx.Observable.fromPromise(Axios.get('http://47.90.126.26/api/china'))
+		.retryWhen(req => {
+			console.log('<retry>', new Date().toLocaleString())
+			return req.delay(1000).take(5)
+		})
+		.subscribe({
+			closed: true,
+			next: res => {
+				if (res.data instanceof Array) {
+					console.log('<response-success>')
+					const provinces = (res.data || []).filter((_, i) => i <= 1)
+					store.next(state => ({
+						isLoading: false,
+						provinces
+					}))
+				} else {
+					console.log('<response-error>')
+					store.next(state => ({
+						isLoading: false,
+						provinces: []
+					}))
+				}
+			},
+			error: err => {
+				console.log('<network-error>')
+				store.next(state => ({
+					isLoading: false
+				}))
+			},
+			complete: () => {
+				console.log('<complete>')
+
+				store.next(state => ({
+					isLoading: false
+				}))
+			}
+		})
+}
+
+const s3 = getProvince()
+// s3.unsubscribe()
