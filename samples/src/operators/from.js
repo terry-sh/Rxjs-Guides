0d0f26f23e6fc12528455d76fc45051aa7728960
o-jules <jules3in@gmail.com>
2018-03-03 10:18:38 +0800

    update: `from` operator

--- samples/src/operators/from.js
+++ samples/src/operators/from.js
@@ -1,15 +1,34 @@
 const Rx = require('rxjs')
 
-/**
- * from
- * 将 array-like 的对象转化成 event stream
- */
-const obs = Rx.Observable.from([1, 2, 3, 4])
 
-obs.subscribe(i => {
+const o1 = i => {
 	console.log('<1>', i)
+}
+const o2 = i => {
+	console.log('<2>', i)
+}
+
+
+/**
+ * @function
+ * from
+ * 将 array 或者 array-like 的对象转化成 event stream
+ */
+
+
+// 数组(array)作为参数
+const obs1 = Rx.Observable.from([1, 2, 3, 4])
+obs1.subscribe(o1)
+obs1.subscribe(o2)
+
+// array-like 对象作为参数
+const obs2 = Rx.Observable.from({
+	0: 'a',
+	1: 'b',
+	2: 'c',
+	3: 'd',
+	length: 4,
 })
 
-obs.subscribe(i => {
-	console.log('<2>', i)
-})
+obs2.subscribe(o1)
+obs2.subscribe(o2)
