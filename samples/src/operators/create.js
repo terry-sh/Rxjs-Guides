2390e4e508ed5954937c548d5be32895d3d6f0e4
o-jules <jules3in@gmail.com>
2018-03-04 15:00:00 +0800

    update: operator `create`

--- samples/src/operators/create.js
+++ samples/src/operators/create.js
@@ -1,6 +1,6 @@
 const Rx = require('rxjs')
 
-const obs = Rx.Observable.create(function(observer) {
+const obs = Rx.Observable.create(function (observer) {
 	observer.next(1)
 	observer.next(2)
 	observer.next(3)
@@ -21,3 +21,33 @@ obs.subscribe({
 // 2
 // 3
 // complete
+
+
+// async 異步
+const asyncObs = Rx.Observable.create((observer) => {
+	let i = 0
+
+	const timer = setInterval(() => {
+		observer.next(i++)
+	}, 1000)
+
+	// return `unsubscribe`
+	return () => {
+		clearInterval(timer)
+		observer.complete()
+	}
+})
+
+const obs2 = asyncObs.subscribe({
+	next: i => {
+		console.log(i)
+	},
+	error: err => {},
+	complete: () => {
+		console.log('async complete')
+	}
+})
+
+setTimeout(() => {
+	obs2.unsubscribe()
+}, 4000)
