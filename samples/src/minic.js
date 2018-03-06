3a36a407f9d62446a4a116c56ef0592e596ccebb
o-jules <jules3in@gmail.com>
2018-03-06 23:29:32 +0800

    add: minic.js

---
+++ samples/src/minic.js
@@ -0,0 +1,52 @@
+/**
+ * 模仿 Observable 的簡單 JavaScript
+ */
+
+function nop() { /** no-op function **/ }
+
+function Observable(obserableCreator) {
+
+	return {
+		subscribe(observer) {
+			return {
+				unsubscribe: obserableCreator(observer) || nop
+			}
+		}
+	}
+
+}
+
+const observable = Observable((observer) => {
+	let index = 0
+
+	const timer = setInterval(() => {
+		observer.next(++index)
+
+		if (index >= 20) {
+			observer.complete()
+			clearInterval(timer)
+		}
+
+	}, 1000)
+
+	return () => {
+		clearInterval(timer)
+	}
+
+})
+
+const subscription = observable.subscribe({
+	next(val) {
+		console.log(val)
+	},
+	error(err) {
+		//
+	},
+	complete() {
+		console.log('complete')
+	}
+})
+
+setTimeout(() => {
+	subscription.unsubscribe()
+}, 6000)
