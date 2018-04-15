31c642c9965b484a72a121f7ec6ac1585532c03e
o-jules <jules3in@gmail.com>
2018-04-15 23:46:03 +0800

    update operator example forkJoin

--- samples/src/operators/combination/forkJoin.js
+++ samples/src/operators/combination/forkJoin.js
@@ -13,3 +13,17 @@ Rx.Observable
 		const duration = (new Date() - start) / 1000
 		console.log(`takes ${duration} seconds`)
 	})
+
+
+	// 等价于：
+	Rx.Observable
+	.zip(
+		Rx.Observable.interval(200).take(30).last(),
+		Rx.Observable.interval(300).take(15).last(),
+		(n, m) => n + m
+	)
+	.subscribe(i => {
+		console.log(i) // 43
+		const duration = (new Date() - start) / 1000
+		console.log(`takes ${duration} seconds`)
+	})
