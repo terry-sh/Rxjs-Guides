0ba846fe91265abeaf0e7eae5cd805794ed108a0
o-jules <jules3in@gmail.com>
2019-02-11 17:28:08 +0800

    update samples/observable.js

--- samples/src/observable.js
+++ samples/src/observable.js
@@ -1,22 +1,29 @@
-var Rx = require('rxjs')
+var Rx = require("rxjs")
+
+let _observer = null
 
 const observable = Rx.Observable.create(observer => {
-	observer.next(1);
-	observer.next(2);
-	observer.next(3);
-	observer.complete();
-});
+  _observer = observer
 
-const subscription = observable.subscribe({
-	next: (val) => {
-		setTimeout(() => {
-			console.log(val);
-		}, 1000)
-	},
-	complete: () => {
-		console.log('complete')
-	},
-	error: () => {
-		console.log('error')
-	}
-});
+  observer.next(1)
+  observer.next(2)
+  observer.next(3)
+})
+
+const subscribe = tag => {
+  observable.subscribe({
+    next: val => console.log(`<${tag}>`, val),
+    complete: () => console.log(`<${tag}>`, '[[complete]]')
+  })
+}
+
+// 如果注释掉该行，下面的代码执行时，_observer 为 null
+subscribe(1)
+
+_observer.next("o1 - " + Math.random())
+_observer.next("o2 - " + Math.random())
+
+subscribe(2)
+subscribe(3)
+
+_observer.complete()
