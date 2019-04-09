f831c653e16f5d9799ee3550bf16c63caa93d346
o-jules <jules3in@gmail.com>
2019-04-09 11:30:22 +0800

    upgrade: skip operator

---
+++ docs/operators/skip.md
@@ -0,0 +1,10 @@
+# `skip`
+
+Skip certain times. The observable do not need to `complete`.
+
+```ts
+/**
+ * @param skipTimes the times to skip
+ */
+function skip(skipTimes: number);
+```
--- samples/src/operators/filter/skip.js
+++ samples/src/operators/filter/skip.js
@@ -1,14 +1,40 @@
-const Rx = require('rxjs')
+const { Observable, of, timer } = require("rxjs")
+const { skip, filter, take } = require("rxjs/operators")
 
-Rx.Observable.of(1,2,3,4,5).skip(2).subscribe(i => {
-	console.log(i)
-})
+of(1, 2, 3, 4, 5)
+  .pipe(skip(2))
+  .subscribe(i => {
+    console.log(i)
+  })
 
-Rx.Observable.create(obs => {
-	obs.next(1)
-	obs.next(2)
-	obs.next(3)
-	obs.next(4)
-}).skip(2).subscribe(i => {
-	console.log('no complete:', i)
+Observable.create(obs => {
+  obs.next(1)
+  obs.next(2)
+  obs.next(3)
+  obs.next(4)
 })
+  .pipe(skip(2))
+  .subscribe(i => {
+    console.log("no complete is okay:", i)
+  })
+
+{
+  // won't handle user's click until the user clicks more than four times
+  const userClick = timer(0, 1000) // simulates user clicking
+  let handling = false
+  userClick
+    .pipe(
+      skip(3),
+      filter(() => !handling),
+      take(3)
+    )
+    .subscribe(i => {
+      console.log("allow click", i)
+      handling = true
+
+      // do something asynchorously
+      setTimeout(() => {
+        handling = false
+      }, 2400)
+    })
+}
