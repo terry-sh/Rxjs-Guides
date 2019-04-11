0136331178abc3d3e2bbead173f2b540a132245b
o-jules <jules3in@gmail.com>
2019-04-11 16:47:49 +0800

    update; sample timer

--- samples/src/operators/creation/timer.js
+++ samples/src/operators/creation/timer.js
@@ -1,9 +1,22 @@
-const Rx = require('rxjs')
+const { timer } = require("rxjs")
+const { take } = require("rxjs/operators")
 
-Rx.Observable.timer(1000).subscribe(i => {
-	console.log('timeout', i)
+const label1 = "timeout-1"
+console.time(label1)
+timer(new Date(Date.now() + 1000 * 2.3)).subscribe(i => {
+  console.log(label1, i)
+  console.timeEnd(label1)
 })
 
-Rx.Observable.timer(2000, 500).take(2).subscribe(i => {
-	console.log('interval', i)
+const label2 = "timeout-2"
+console.time(label2)
+timer(1000).subscribe(i => {
+  console.log(label2, i)
+  console.timeEnd(label2)
 })
+
+timer(3000, 500)
+  .pipe(take(3))
+  .subscribe(i => {
+    console.log("interval", i)
+  })
